f46d9978e1f85852a97825f71c498d1c0aa03998
Bart <barpahl@publicisgroupe.net>
2020-11-15 20:14:58 +0000

    fixing merge conflicts

--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:nightingale_v1/screens/authenticate/sign_in_page.dart';
 import 'package:nightingale_v1/screens/medicine/medicine_page.dart';
+import 'package:nightingale_v1/screens/schedule/scheduling_page.dart';
+import 'package:nightingale_v1/screens/doctor/doctor_page.dart';
 import '../authenticate/sign_in_page.dart';
 import 'package:nightingale_v1/services/auth.dart';
+import 'package:alan_voice/alan_voice.dart';
 
 
 class HomePage extends StatelessWidget {
@@ -28,6 +31,59 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final AuthService _auth = AuthService();
+
+
+void _navigateTo(String screen) {
+	switch (screen) {
+		case "Home":
+			Navigator.push(
+				context,
+				MaterialPageRoute(builder: (context) => HomePage()),
+			);
+			break;
+		case "Medicine":
+			Navigator.push(
+				context,
+				MaterialPageRoute(builder: (context) => MedicinePage()),
+			);
+			break;
+    case "Schedule":
+			Navigator.push(
+				context,
+				MaterialPageRoute(builder: (context) => SchedulingPage()),
+			);
+			break;
+      	case "Medicine":
+			Navigator.push(
+				context,
+				MaterialPageRoute(builder: (context) => DoctorPage()),
+			);
+			break;
+		default:
+			print("Unknown screen: $screen");
+	}
+}
+
+void _handleCommand(Map<String, dynamic> command) {
+	switch (command["command"]) {
+		case "navigation":
+			_navigateTo(command["route"]);
+			break;
+		default:
+			debugPrint("Unknown command: ${command}");
+	}
+}
+
+  _MyHomePageState() {
+    //Init Alan with sample project id
+    AlanVoice.addButton(
+        "5b42ec32b29e02864b0ddbc2a565aec92e956eca572e1d8b807a3e2338fdd0dc/stage",
+        buttonAlign: AlanVoice.BUTTON_ALIGN_LEFT);
+
+    //Add listener for command event
+		AlanVoice.callbacks.add((command) => _handleCommand(command.data));
+  }
+
 
   @override
   Widget build(BuildContext context) {
